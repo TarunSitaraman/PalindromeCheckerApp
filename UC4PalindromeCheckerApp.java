6f82fae81cdca1155240334c9f6f9d0da9915e16
Tarun Sitaraman <mfsbyo@gmail.com>
2026-02-18 10:22:38 +0530

    UC4

---
+++ UC4PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+
+public class UC4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "madam";
+        char[] charArray = input.toCharArray();
+
+        int start = 0;
+        int end = charArray.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("Input: " + input);
+        if (isPalindrome) {
+            System.out.println("Result: The string is a palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
+    }
+}
